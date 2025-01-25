51b237bcb30bef38b85b9afdea20db2fcd2a4a0a
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-25 22:56:38 +0900

    waaaaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './global.css';
 import { GoogleAnalytics } from '@next/third-parties/google';
-import Header from '@/components/layouts/header';
+import Header from '@/components/Header';
 import localFont from 'next/font/local';
 import { Viewport } from 'next';
-import Footer from '@/components/layouts/footer';
+import Footer from '@/components/Footer';
 
 const SF_Pro = localFont({src: './fonts/sf-pro-text_regular.woff2'});
 
