6ae32842ef2a8e25de90da4663f30280350b18c3
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-25 22:49:43 +0900

    delete onion mirror link

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function Footer() {
     return (
         <div className="text-center bg-blue-500 text-white text-lg py-2">
-            <Link className="text-gray-200" href={"http://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>Onion Mirror</Link><br/>
+            {/*<Link className="text-gray-200" href={"http://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>Onion Mirror</Link><br/>*/}
             Copyright 2025 manaken0522 All rights reserved.
         </div>
     )
